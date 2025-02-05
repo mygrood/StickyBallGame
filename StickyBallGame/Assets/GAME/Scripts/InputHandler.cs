ed0c042cd33e00a41f54a0441ca6ae538050df7a
mygrood <aamikhailovv@gmail.com>
2025-02-05 15:44:41 +0300

    implement inputhandler

---
+++ StickyBallGame/Assets/GAME/Scripts/InputHandler.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            HandleTap(Input.mousePosition);
+        }
+        else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            HandleTap(Input.GetTouch(0).position);
+        }
+    }
+
+    private void HandleTap(Vector2 position)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(position), Vector2.zero);
+
+        if (hit.collider != null)
+        {
+            Debug.Log(hit.collider.gameObject.name);
+
+            // Если объект имеет интерфейс IInteractable, вызываем метод
+            if (hit.transform.TryGetComponent(out IInteractable interactable))
+            {
+                interactable.OnTap();
+            }
+        }
+        else
+        {
+            Debug.Log("Ray did not hit any object.");
+        }
+    }
+}
