43f01bc1d8e211b2b9b8efa881a6d6523d3dcf9b
mygrood <aamikhailovv@gmail.com>
2025-02-06 18:34:50 +0300

    simple UI

--- StickyBallGame/Assets/GAME/Scripts/PlayerHealth.cs
+++ StickyBallGame/Assets/GAME/Scripts/PlayerHealth.cs
@@ -1,11 +1,14 @@
+using System;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private int maxHealth = 5;
+    public int MaxHealth { get { return maxHealth; } }
     public int СurrentHealth { get; private set; }
     public bool IsDead => СurrentHealth <= 0;
 
+    public event Action<int> OnHealthChanged;
     private void Awake()
     {
         СurrentHealth = maxHealth;
@@ -14,6 +17,7 @@ public class PlayerHealth : MonoBehaviour
     public void TakeDamage(int damage)
     {
         СurrentHealth = Mathf.Clamp(СurrentHealth - damage, 0, maxHealth);
+        OnHealthChanged?.Invoke(СurrentHealth);
         if (IsDead)
         {
             Die();
@@ -22,8 +26,8 @@ public class PlayerHealth : MonoBehaviour
   
     public void Heal(int amount)
     {
-        
         СurrentHealth = Mathf.Clamp(СurrentHealth +amount, 0, maxHealth);
+        OnHealthChanged?.Invoke(СurrentHealth);
     }
 
    private void Die()
---
+++ StickyBallGame/Assets/GAME/Scripts/UI/HealthUI.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    [SerializeField] private GameObject heartPrefab;
+    [SerializeField] private Sprite fullHeart;
+    [SerializeField] private Sprite emptyHeart;
+
+    PlayerHealth playerHealth;
+    private Image[] hearts;
+
+    private void Start()
+    {
+        playerHealth = GameManager.Instance.player.gameObject.GetComponent<PlayerHealth>();
+        playerHealth.OnHealthChanged += UpdateUI;
+
+        InitializeUI();
+    }
+
+    private void InitializeUI()
+    {
+        int maxHealth = playerHealth.MaxHealth;
+        hearts = new Image[maxHealth];
+        for (int i = 0; i < maxHealth; i++)
+        {
+            GameObject heartObject = Instantiate(heartPrefab, transform);
+            hearts[i] = heartObject.GetComponent<Image>();
+        }
+        UpdateUI(playerHealth.СurrentHealth);
+    }
+
+    private void UpdateUI(int health)
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+                hearts[i].sprite = fullHeart;
+            else
+                hearts[i].sprite = emptyHeart;
+        }
+    }
+}
